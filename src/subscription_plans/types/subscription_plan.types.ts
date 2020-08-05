f314801331260484cab053573c82865602ecc28b
asimashfaq <asimizb4@gmail.com>
2020-08-05 16:49:36 +0500

    [SubscriptionPlan]: Create typings for the CRUD operation

--- src/subscription_plans/types/subscription_plan.types.ts
+++ src/subscription_plans/types/subscription_plan.types.ts
@@ -1,13 +1,22 @@
+import {
+  IChangeResponseInfo,
+  IPagination,
+  IResponseInfo,
+} from '../..//shared/interfaces/common.interfaces';
+import { SortProperties } from '../../shared/types/sort.types';
 import { ISubscriptionPlan } from '../interfaces/subscription_plan.interface';
 
-export enum SubscriptionPlanDurationEnum {
+enum SubscriptionPlanDurationEnum {
   DAY = 'DAY',
   WEEK = 'WEEK',
   MONTH = 'MONTH',
   YEAR = 'YEAR',
 }
 export type SubscriptionDuration = keyof typeof SubscriptionPlanDurationEnum;
+
 export type SubscriptionPlan = ISubscriptionPlan;
+
+// Type for creating Subscription Plan
 export type SubscriptionInputPayload = Required<
   Pick<
     SubscriptionPlan,
@@ -15,3 +24,31 @@ export type SubscriptionInputPayload = Required<
   >
 > &
   Pick<SubscriptionPlan, 'description' | 'trail_period' | 'trail_duration'>;
+
+// Type of Subscription Plan Properties used for sorting
+// id | price
+type SubscriptionPlanSortKeys = Extract<
+  keyof Pick<SubscriptionPlan, 'id' | 'price'>,
+  string
+>;
+
+// Transform the property type to SortDirection
+// {id: SortDirection}
+type SubscriptionPlanSort = SortProperties<SubscriptionPlanSortKeys>;
+
+// Type for getting Subscription Plan
+export type SubscriptionPlanFilter = Partial<
+  Pick<SubscriptionPlan, 'id' | 'code' | 'slug'>
+> &
+  Pick<IPagination, 'limit' | 'skip'> & { sort_by: SubscriptionPlanSort };
+
+// Type for Updating Subscription Plan
+export type SubscriptionPlanUpdatePayload = SubscriptionInputPayload;
+
+export type SubscriptionPlanResponse = IResponseInfo<SubscriptionPlan>;
+export type DeleteSubscriptionPlanResponse = IChangeResponseInfo<
+  SubscriptionPlan
+>;
+export type UpdateSubscriptionPlanResponse = IChangeResponseInfo<
+  SubscriptionPlan
+>;
