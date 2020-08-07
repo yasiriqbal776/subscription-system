b19f17a79e1e96e14a6c8901b067c03b2f165358
asimashfaq <asimizb4@gmail.com>
2020-08-07 15:40:27 +0500

    [Services]: Init Module

---
+++ src/services/IService.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-default-export
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IService {}
+
+export const ServiceSymbol = Symbol('Service');
+
+// eslint-disable-next-line import/no-default-export
+export default IService;
---
+++ src/services/inversify.config.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/tslint/config */
+import 'reflect-metadata';
+
+import { Container, ContainerModule, interfaces } from 'inversify';
+import _ from 'lodash';
+
+import { ConfigService } from '../config/config.service';
+import { ServiceSymbol } from './IService';
+import { ServicesApp } from './services.app';
+
+const bindings = new ContainerModule((bind: interfaces.Bind) => {
+  _.each([ConfigService], (Service: any) => {
+    bind(Service).to(Service).inSingletonScope();
+
+    bind<any>(ServiceSymbol).toFactory<any>((context) =>
+      context.container.get<any>(Service),
+    );
+  });
+  bind<ServicesApp>(ServicesApp).toSelf().inSingletonScope();
+});
+
+const container = new Container();
+
+container.load(bindings);
+
+export const servicesContainer = container;
